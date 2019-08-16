0a494a528dbf634ec9569b164423ddaba307d0bd
Silva <darlan.ricardo.silva@accenture.com>
2019-08-16 10:08:12 -0300

    Utilizando a diretiva *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  photos = [
+    {
+      url:'https://images.uncyc.org/pt/thumb/7/7c/Homerf.png/300px-Homerf.png',
+      description:'Hommer'
+    },
+    {
+      url:'http://d26lpennugtm8s.cloudfront.net/stores/802/372/products/bart1-b5a7e6b5fa4a4e179e15266685305387-1024-1024-eeac8a8ea17967548d15441973395151-640-0.png',
+      description:'Bart'
+    }
+  ];
 }
 
