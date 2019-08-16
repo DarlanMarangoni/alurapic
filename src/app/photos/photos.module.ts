23583bf575d2e29d5fc3acf46d269afc8b79eb39
Silva <darlan.ricardo.silva@accenture.com>
2019-08-16 17:33:05 -0300

    Utilizando submodulos para organizar nossa app

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotosListComponent } from './photos-list/photos-list.component';
-import { PhotosFormComponent } from './photos-form/photos-form.component';
-import { PhotosComponent } from './photos-list/photos/photos.component';
-import { FilterByDescription } from './photos-list/photos/filter-by-description.pipe';
-import { LoadButtonComponent } from './photos-list/load-button/load-button.component';
+import { PhotosFormModule } from './photos-form/photos-form.module';
+import { PhotoListModule } from './photos-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotosListComponent,
-        PhotosFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
+   
     imports: [
+        PhotoModule,
+        PhotosFormModule,
+        PhotoListModule,
         HttpClientModule,
         CommonModule
     ]
