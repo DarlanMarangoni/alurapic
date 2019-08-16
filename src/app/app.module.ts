e62372fe347c32b135a63cd7f3aa25972610bb70
Silva <darlan.ricardo.silva@accenture.com>
2019-08-16 10:59:26 -0300

    HttpClient e injecao de dependencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
