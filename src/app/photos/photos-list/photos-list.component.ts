b3aea687ab4e3f6ec293f004c37832c785208c4a
Silva <darlan.ricardo.silva@accenture.com>
2019-08-16 16:31:36 -0300

    Melhorando o desempenho da nossa app utilizando RxJS e seu Subjct

--- src/app/photos/photos-list/photos-list.component.ts
+++ src/app/photos/photos-list/photos-list.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Photo } from '../photo/photo';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from  'rxjs/operators'
 
 @Component({
   selector: 'ap-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
+export class PhotosListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor( private activatedRoute: ActivatedRoute) { 
 
@@ -18,6 +21,13 @@ export class PhotosListComponent implements OnInit {
 
   ngOnInit() {
     this.photos = this.activatedRoute.snapshot.data['photos'];      
+    this.debounce
+        .pipe(debounceTime(300))
+        .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void{
+      this.debounce.unsubscribe();
   }
 
 }
