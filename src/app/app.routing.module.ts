91b6468662110e534374364559161e62e1052db7
Silva <darlan.ricardo.silva@accenture.com>
2019-08-16 13:39:03 -0300

    Redirecionando endereco inexistente

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
 
 import { PhotosFormComponent } from './photos/photos-form/photos-form.component';
-import { PhotosListComponent } from './photos/photos-list/photos-list.component'
+import { PhotosListComponent } from './photos/photos-list/photos-list.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
     {path: 'user/flavio', component: PhotosListComponent},
-    {path: 'p/add', component: PhotosFormComponent}
+    {path: 'p/add', component: PhotosFormComponent},
+    {path: '**', component: NotFoundComponent}
 ];
 @NgModule({
     imports: [
