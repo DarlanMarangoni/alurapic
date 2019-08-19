c90fa80634437f0415ecc99f868d986db6c104c4
Silva <darlan.ricardo.silva@accenture.com>
2019-08-19 12:53:22 -0300

    Enviando credenciais para a API, implementando autenticação

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -8,7 +9,7 @@ export class SignInComponent implements OnInit{
     
     loginForm: FormGroup;
     
-    constructor(private formBuilder: FormBuilder){
+    constructor(private formBuilder: FormBuilder, private authService: AuthService){
         
     }
 
@@ -17,5 +18,19 @@ export class SignInComponent implements OnInit{
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+    
+    login(){
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                ()=> console.log('autenticado'),
+                err =>{
+                    this.loginForm.reset();
+                    alert('Invalid user name or password')
+                });
     }
 }
