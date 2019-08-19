05d79cc425b52b5a946834af210f62a2a01910e6
Silva <darlan.ricardo.silva@accenture.com>
2019-08-19 12:28:04 -0300

    Componetizando mensagens de validação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessages/vmessage.module';
 
 @NgModule({
     declarations: [SignInComponent],
     imports: [
         ReactiveFormsModule,
-        CommonModule
+        CommonModule,
+        VMessageModule
     ]
 })
 
---
+++ src/app/shared/components/vmessages/vmessage.module.ts
@@ -0,0 +1,10 @@
+import { NgModule, Input } from '@angular/core';
+import { VMessageComponent } from './vmessage.component';
+
+@NgModule({
+    declarations: [VMessageComponent],
+    exports: [VMessageComponent]
+})
+export class VMessageModule{
+    
+}
