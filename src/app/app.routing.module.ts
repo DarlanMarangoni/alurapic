c6c23b06abb6dcd13c8b171eeba9dee2bfb6b7ea
Silva <darlan.ricardo.silva@accenture.com>
2019-08-20 13:02:12 -0300

    Redirecionando usuario logado para fotos

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,10 +6,13 @@ import { PhotosListComponent } from './photos/photos-list/photos-list.component'
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotolistResolver } from './photos/photos-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
     {
-        path: '', component: SignInComponent
+        path: '', 
+        component: SignInComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'user/:userName', 
