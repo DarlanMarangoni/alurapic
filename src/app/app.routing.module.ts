5b7ae87e33f62a0f7eb09f921030a244eb405cd8
Silva <darlan.ricardo.silva@accenture.com>
2019-08-21 10:50:17 -0300

    Agora sim parametrizei

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
         canActivate: [AuthGuard]
     },
     {
-        path: 'p/1', 
+        path: 'p/:photoId', 
         component: PhotoDetailsComponent
     },
     {
