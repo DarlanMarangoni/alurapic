1a283be6fc7482f62195d6b1d8bd938e5c2cbf6d
Silva <darlan.ricardo.silva@accenture.com>
2019-08-21 08:53:04 -0300

    Implementando upload de fotos

--- src/app/photos/photos-form/photos-form.module.ts
+++ src/app/photos/photos-form/photos-form.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotosFormComponent } from './photos-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/components/vmessages/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotosFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
     ]
 })
 export class PhotosFormModule{
