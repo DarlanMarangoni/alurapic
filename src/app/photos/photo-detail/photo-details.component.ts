562dc8a7e7a41fd3f6afca0a98dc7ca83c683da6
Silva <darlan.ricardo.silva@accenture.com>
2019-08-21 16:15:37 -0300

    Removendo alerta durante mudança de rota

--- src/app/photos/photo-detail/photo-details.component.ts
+++ src/app/photos/photo-detail/photo-details.component.ts
@@ -4,6 +4,7 @@ import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -16,7 +17,8 @@ export class PhotoDetailsComponent implements OnInit{
     constructor(private route: ActivatedRoute, 
                 private photoService: PhotoService,
                 private router: Router,
-                private alertService: AlertService){
+                private alertService: AlertService,
+                private userService: UserService){
         
     }
     
@@ -29,12 +31,12 @@ export class PhotoDetailsComponent implements OnInit{
         this.photoService
             .removePhoto(this.photoId)
             .subscribe(() => {
-                this.alertService.success("Photo removed");
-                this.router.navigate(['']);            
+                this.alertService.success("Photo removed", true);
+                this.router.navigate(['/user', this.userService.getUserName()]);            
             },
             err =>{
                 console.log(err);
-                this.alertService.warning('Could you not delete the photo');
+                this.alertService.warning('Could you not delete the photo', true);
             });
     }
 }
